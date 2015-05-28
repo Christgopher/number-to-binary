c1a68b20acf127eb03b9316719f06df4eff1d291
chris lecompte <student@epicodus.com>
2015-05-28 14:43:24 -0700

    accidently erased my local git repo, but hey, its finished

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,39 @@
-var countUpBy = function(upTo, upBy) {
-  var numbers = [];
-  for (var i = upBy; i <= upTo; i = i + upBy) {
-    numbers.push(i);
-  }
-  return numbers;
-};
+var wordOrder= function(sentence) {
+  var words = {};
+  var sentenceArray = sentence.split(" ")
 
-$(document).ready(function() {
+  sentenceArray.forEach(function(word) {
+    if (Object.keys(words).indexOf(word) === -1) {
+      words[word] = 1;
+    } else {
+      words[word] += 1;
+    }
+  });
 
-  $("form#numbers").submit(function(event) {
-    var upTo = parseInt($("input#upTo").val());
-    var upBy = parseInt($("input#upBy").val());
-    var result = countUpBy(upTo, upBy).join(" ");
-    $(".numbers").text(result);
+  var sortable = []
+  for (var word in words)
+  sortable.unshift([word, words[word]])
+  sortable.sort(function(a, b) {return a[1] - b[1]})
+
+  var finalArray = []
+
+  sortable.forEach(function(property) {
+    finalArray.push((property[0].toString()) + " " + (property[1]).toString())
+
+  });
+
+  return finalArray.reverse();
+}
+
+$(function() {
+
+  $("form#sentence").submit(function(event) {
+    var sentence = $("input#sentence").val();
+    var result = wordOrder(sentence).reverse();
+    result.forEach(function(property) {
+      $(".counts").prepend("<li>" + property + "</li>")
+    });
+
 
     event.preventDefault();
   });
--- spec/specs.js
+++ spec/specs.js
@@ -1,13 +1,17 @@
-describe('countUpBy', function() {
-  it("counts up to 5 by one", function() {
-    expect(countUpBy(5, 1)).to.eql([1, 2, 3, 4, 5])
+describe('wordStorer', function() {
+  it("makes a new hash entry for every word", function() {
+    expect(wordOrder("i am groot")).to.eql(["i am groot"])
   });
 
-  it("counts up to 10 by two", function() {
-    expect(countUpBy(10, 2)).to.eql([2, 4, 6, 8, 10])
+  it("counts the number of times a word appears", function() {
+    expect(wordOrder("i am groot groot groot")).to.eql(["i am groot"])
   });
 
-  it("counts up to 50 by seven", function() {
-    expect(countUpBy(50, 7)).to.eql([7, 14, 21, 28, 35, 42, 49])
+  it("sorts things", function() {
+    expect(wordOrder("i am groot groot groot")).to.eql("groot, 3. i, 1. am, 1.")
+  });
+
+  it("sorts things", function() {
+    expect(wordOrder("i i i i i  a k groot groot groot groot")).to.equal(["i 5", "groot 4", "a 1", "i 1"])
   });
 });
